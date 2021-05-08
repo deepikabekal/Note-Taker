3147c04935513c82d751346dc3972723b0a70e22
Deepika Bekal <deepika.bekal2@gmail.com>
2021-05-08 10:43:40 +0400

    html routes added

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-//import express package
+//import packages
 const express = require ('express');
+const path = require ('path');
 
 //instantiate server
 const app = express();
 
 //declare PORT
 const PORT = process.env.PORT || 3001;
+
+//creating a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //make the server listen
 app.listen (PORT, () => {
