34d57fb7f5b6f3669ed796d6aeea5c936d2e57d4
Deepika Bekal <deepika.bekal2@gmail.com>
2021-05-11 05:23:30 +0400

    fixed the api route issue

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,40 +1,60 @@
 //import packages
-const express = require('express');
-const fs = require ('fs');
-const path = require ('path');
-//const uuid = require ('uuid');
-const { v4: uuidv4 } = require('uuid');
+const express = require ('express');
 const router = express.Router();
-const { data } = require('../db/db.json');
+const fs = require("fs");
+const { notes } = require("../db/db.json");
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
+//function to create new notes
+function createNewNote(body, notes) {
+  const note = body;
+  //console.log(note);
+  notes.push(note);
 
-//creating new note
-function createNewNote(body, data) {
-    const userInput = body;
-    console.log(userInput);
-    data.push(userInput);
-    fs.writeFileSync(
-        path.join(__dirname, '../db/db.json'), 
-        JSON.stringify({ data })
-    );
-    return userInput;
-    
+  //sending the notes data to the json file
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify({ notes }, null, 2)
+  );
+  return note;
+}
+
+//function to delete the notes
+function deleteNote(id, notes) {
+  let noteStatus = false;
+  let count = 0;
+  while (count < notes.length || !status) {
+    if (notes[count].id === id) {
+      notes.splice(count, 1);
+      noteStatus = true;
+      fs.writeFileSync(
+        path.join(__dirname, "../../db/db.json"),
+        JSON.stringify({ notes }, null, 2)
+      );
+    } else {
+      count++;
+    }
+  }
 }
 
 //return saved notes
-router.get('/notes', (req,res) => {
-    //console.log("I have been hit")
-        return res.json(data);
+router.get("/notes", (req, res) => {
+  res.json(notes);
 });
 
 //create new note, save ad return
-router.post('/notes', (req, res) => {
-    let notes = createNewNote(req.body, data)
-    res.json(data);    
+router.post("/notes", (req, res) => {
+  req.body.id = uuidv4();
+  let note = createNewNote(req.body, notes);
+  res.json(notes);
 });
 
-
-
-
+//delete the selected note
+router.delete("/notes/:id", (req, res) => {
+  let id = req.params.id;
+  deleteNote(id, notes);
+  res.json(notes)
+});
 
 module.exports = router;
